16a63f4902e5d96314c8b0925901afd9218e8710
Austin <austind0125@gmail.com>
2026-09-09 22:40:36 -0400

    Add header comment and instroduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Austin Dugger
+* Course: CSCI 1250, Section 002
+* Date: September 9, 2026
+* Description: This program prints "Hello, World!" to the console.
+*/
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Austin Dugger.");
+Console.WriteLine("This is my first program at ETSU");
